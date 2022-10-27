d579ae86fc21c27b8b814dd88d54990e6d00d01e
gregdiniz13 <gdiniz13@gmail.com>
2022-10-27 13:26:49 -0300

    Primeiro Commit das aulas de JavaScript

---
+++ DominandoOJavaScript/aula06.js
@@ -0,0 +1,21 @@
+/*String
+
+- Cadeia de caracteres
+
+*/
+
+let myFirsString = "Aqui dentro eu posso escrever o que eu quiser"
+let mySecondString = 'Agora com aspas simples'
+let myThirdString = `Com crase eu também consigo`//template literals ou template strings
+
+let text = "Fulando de tal falou o seguinte: 'Eu aprendi JavaScript.' "
+
+let myTextWithSuperPowers = `Utilizando template literals
+eu consido escrever
+em diversas linhas`
+
+let numberOfPeopleInClass = 34
+
+let howManyInClass = `Tinham exatamente ${numberOfPeopleInClass} pessoas em aula.`
+
+console.log(myTextWithSuperPowers)
